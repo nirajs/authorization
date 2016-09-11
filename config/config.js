366700d0f25d7fef08f822c8a5875e41aa4cacd3
nirajs <nirajkantsinha@gmail.com>
2016-09-11 18:49:20 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,14 +4,14 @@ module.exports = {
     'dbconnect': 'mongodb://localhost/auth',
     'secretKey': 'whatever',
     'facebook': {
-        'AppID': '1130177717107903',
-        'AppSecret' : 'aed376951dd13c25688a2cc7af56329d',
+        'AppID': 'secret',
+        'AppSecret' : 'secrete',
         'callbackURL': 'http://localhost:4000/auth/facebook/callback',
         'profileFields': ['id', 'emails', 'first_name', 'middle_name', 'last_name']
     },
     'google': {
-        'clientID': '430964866951-a7sqdh8cmd8psd6upsoog1lk0jh08ke5.apps.googleusercontent.com',
-        'clientSecret': 'A_7f2MiADRfrAfhTgHWoPJQK',
+        'clientID': ''secret',
+        'clientSecret': 'secret',
         'callbackURL': 'http://localhost:4000/auth/google/callback'
     }
 };
